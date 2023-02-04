dde654c85da6dea7c9ebc9f3452057ebfa5ee1ad
Henrique Bocca Corrêa <correa.henriqueb@gmail.com>
2023-02-03 23:03:19 -0300

    a

---
+++ listaDeTarefas/assets/js/main.js
@@ -0,0 +1,7 @@
+const inputTarefa = document.querySelector('.input-terefa');
+const btnTarefa = document.querySelector('.btn-terefa');
+const tarefa = document.querySelector('.terefa');
+
+btnTarefa.addEventListener('click', function(e) {
+    
+})
